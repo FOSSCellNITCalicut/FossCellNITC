35493cc00df2d76e3c7219e3aacb05afc334e4fe
joelrobin18 <joelrobin1818@gmail.com>
2023-08-01 02:54:48 +0530

    Team Members added

--- src/Components/Team/team.js
+++ src/Components/Team/team.js
@@ -1,5 +1,6 @@
-import img1 from "../../images/img1.jpg"
-import img2 from "../../images/img2.jpeg"
+import { Ameen,Vimal,Vedasree,Sree,Sinadin,Simmon,Rajat,PranavNair,
+Pranav,Piyush,Neena,Jaseem,Afthab,Madhav,Juby,John,JoelRobin,JoelJohn,Hemanth,Hadif,
+Cliford,Aabid,Abin,Afraz,Alen,Aravind,Arjun,Bhukya } from "./teamMem";
 import './team.css';
 import { useState } from "react";
 
@@ -11,6 +12,236 @@ function WholeteamPage() {
         setToggleState(index);
     };
 
+    const office = [
+        {
+            id:1,
+            name:"Mohamed Afthab",
+            position:"Secretary",
+            image:Afthab
+        },
+        {
+            id:2,
+            name:"Madhav Prabhu C M",
+            position:"Joint Secretary",
+            image:Madhav
+        },
+        {
+            id:3,
+            name:"Mohammed Ameen",
+            position:"Treasurer",
+            image:Ameen
+        },
+        {
+            id:4,
+            name:"Joel Robin P",
+            position:"Event Coordinator",
+            image:JoelRobin
+        },
+        {
+            id:5,
+            name:"Sreevalsan S",
+            position:"Event Coordinator",
+            image:Sree
+        },
+        {
+            id:6,
+            name:"Neena M S",
+            position:"Content and Outreach",
+            image:Neena
+        },
+        {
+            id:7,
+            name:"Hemanth Chodisetti",
+            position:"Content and Outreach",
+            image:Hemanth
+        },
+        {
+            id:8,
+            name:"Afraz Ahmed",
+            position:"Marketing Head",
+            image:Afraz
+        },
+        {
+            id:9,
+            name:"Joel George John",
+            position:"Media Head",
+            image:JoelJohn
+        },
+    ]
+
+    const alumni =[
+            {
+            id:1,
+            name:"Abin Latheef",
+            position:"Alumni",
+            image:Abin
+        },
+        {
+            id:2,
+            name:"Aravind I M",
+            position:"Alumni",
+            image:Aravind
+        },
+        {
+            id:3,
+            name:"Arjun M Murali",
+            position:"Alumni",
+            image:Arjun
+        },
+        {
+            id:4,
+            name:"Cliford",
+            position:"Alumni",
+            image:Cliford
+        },
+        {
+            id:5,
+            name:"Hadif Yassin Hameed",
+            position:"Alumni",
+            image:Hadif
+        },
+        {
+            id:6,
+            name:"John D",
+            position:"Alumni",
+            image:John
+        },
+        {
+            id:7,
+            name:"Muhammed Jaseem",
+            position:"Alumni",
+            image:Jaseem
+        },
+        {
+            id:8,
+            name:"Rajat",
+            position:"Alumni",
+            image:Rajat
+        },
+        {
+            id:9,
+            name:"Sinadin Shan",
+            position:"Alumni",
+            image:Sinadin
+        },
+        {
+            id:10,
+            name:"Vimal Joseph",
+            position:"Alumni",
+            image:Vimal
+        },
+    ]
+
+    const member =[
+        {
+            id:1,
+            name:"Mohamed Afthab",
+            position:"Secretary",
+            image:Afthab
+        },
+        {
+            id:2,
+            name:"Madhav Prabhu C M",
+            position:"Joint Secretary",
+            image:Madhav
+        },
+        {
+            id:3,
+            name:"Mohammed Ameen",
+            position:"Treasurer",
+            image:Ameen
+        },
+        {
+            id:4,
+            name:"Joel Robin P",
+            position:"Event Coordinator",
+            image:JoelRobin
+        },
+        {
+            id:5,
+            name:"Sreevalsan S",
+            position:"Event Coordinator",
+            image:Sree
+        },
+        {
+            id:6,
+            name:"Neena M S",
+            position:"Content and Outreach",
+            image:Neena
+        },
+        {
+            id:7,
+            name:"Hemanth Chodisetti",
+            position:"Content and Outreach",
+            image:Hemanth
+        },
+        {
+            id:8,
+            name:"Afraz Ahmed",
+            position:"Marketing Head",
+            image:Afraz
+        },
+        {
+            id:9,
+            name:"Joel George John",
+            position:"Media Head",
+            image:JoelJohn
+        },
+        {
+            id:10,
+            name:"Abid Mohamed",
+            position:"Member",
+            image:Aabid
+        },
+        {
+            id:11,
+            name:"Alen Antony",
+            position:"Member",
+            image:Alen
+        },
+        {
+            id:12,
+            name:"Bhukya CHtan Ram",
+            position:"Member",
+            image:Bhukya
+        },
+        {
+            id:13,
+            name:"Juby Johnson",
+            position:"Member",
+            image:Juby
+        },
+        {
+            id:14,
+            name:"Piyush Soni",
+            position:"Member",
+            image:Piyush
+        },
+        {
+            id:15,
+            name:"Pranav Prashant",
+            position:"Member",
+            image:Pranav
+        },
+        {
+            id:16,
+            name:"Pranav R Nair",
+            position:"Member",
+            image:PranavNair
+        },
+        {
+            id:17,
+            name:"Simmon Mathew Shaji",
+            position:"Member",
+            image:Simmon
+        },
+        {
+            id:18,
+            name:"Vedasree Rodi",
+            position:"Member",
+            image:Vedasree
+        }
+    ]
     return <>
 
         <div className="mainport" id="team">
@@ -19,123 +250,37 @@ function WholeteamPage() {
                 <h1 className={"mainhead"}>Meet The Team</h1>
             </div>
 
-
             <div className={toggleState === 1 ? "content  active-content" : "content"}>
-            
+            {office.map((item)=>(
                 <div className={"individual"}>
                     <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"}/>
+                        <img src={item.image} alt="Logo" className={"avatar"}/>
                     </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
+                    <span className={"name"}>{item.name}<br/>{item.position}</span>
                 </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"}/>
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
+            ))}
             </div>
 
-
-
-            <div className={toggleState === 2 ? "content  active-content" : "content"}>
-                
+            <div className={toggleState === 2 ? "content  active-content" : "content"}>  
+                {member.map((item)=>(
                 <div className={"individual"}>
                     <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"}/>
+                        <img src={item.image} alt="Logo" className={"avatar"}/>
                     </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
+                    <span className={"name"}>{item.name}<br/>{item.position}</span>
                 </div>
-                
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img2} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
+            ))}
             </div>
-
-
 
             <div className={toggleState === 3 ? "content  active-content" : "content"}>
-                
+                {alumni.map((item)=>(
                 <div className={"individual"}>
                     <div className="beneath">
-                        <img src={img2} alt="Logo" className={"avatar"}/>
+                        <img src={item.image} alt="Logo" className={"avatar"}/>
                     </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
+                    <span className={"name"}>{item.name}<br/>{item.position}</span>
                 </div>
-                
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img2} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img2} alt="Logo" className={"avatar"}/>
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img1} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img2} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img2} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
-
-                <div className={"individual"}>
-                    <div className="beneath">
-                        <img src={img2} alt="Logo" className={"avatar"} />
-                    </div>
-                    <span className={"name"}>NAME<br/>POSITION</span>
-                </div>
+            ))}
             </div>
 
 
---
+++ src/Components/Team/teamMem.js
@@ -0,0 +1,35 @@
+import Aabid from "../../images/Team/Aabid Mohamed.jpg"
+import Abin from "../../images/Team/Abin_Latheef.jpg"
+import Afraz from "../../images/Team/Afraz Ahmed.jpeg"
+import Alen from "../../images/Team/Alen_Antony.jpg"
+import Aravind from "../../images/Team/Aravind_I_M.png"
+import Arjun from "../../images/Team/Arjun_M_Murali.jpeg"
+import Bhukya from "../../images/Team/Bhukya Chetan Ram.jpg"
+
+import Cliford from "../../images/Team/Cliford.jpg"
+import Hadif from "../../images/Team/Hadif_Yassin_Hameed.JPG"
+import Hemanth from "../../images/Team/Hemanth Chodisetti.jpg"
+import JoelJohn from "../../images/Team/Joel George John.jpg"
+import JoelRobin from "../../images/Team/Joel Robin P.jpg"
+import John from "../../images/Team/John_D.png"
+
+import Juby from "../../images/Team/Juby Johnson.JPG"
+import Madhav from "../../images/Team/Madhav_Prabhu.jpg"
+import Afthab from "../../images/Team/Mohamed_Afthab.jpg"
+import Jaseem from "../../images/Team/Muhammed Jaseem.jpg"
+import Neena from "../../images/Team/Neena_MS.jpg"
+import Piyush from "../../images/Team/Piyush Soni.jpeg"
+import Pranav from "../../images/Team/Pranav Prashant.jpeg"
+
+import PranavNair from "../../images/Team/Pranav R Nair.jpg"
+import Rajat from "../../images/Team/Rajat.jpg"
+import Simmon from "../../images/Team/Simon Shaji.jpeg"
+import Sinadin from "../../images/Team/Sinadin Shan.jpg"
+import Sree from "../../images/Team/Sreevalsan.jpeg"
+import Vedasree from "../../images/Team/Vedasree Rodi.jpeg"
+import Vimal from "../../images/Team/Vimal Joseph.JPG"
+import Ameen from "../../images/Team/Mohammed Ameen.jpg"
+
+export{Ameen,Vimal,Vedasree,Sree,Sinadin,Simmon,Rajat,PranavNair,
+Pranav,Piyush,Neena,Jaseem,Afthab,Madhav,Juby,John,JoelRobin,JoelJohn,Hemanth,Hadif,
+Cliford,Aabid,Abin,Afraz,Alen,Aravind,Arjun,Bhukya}
